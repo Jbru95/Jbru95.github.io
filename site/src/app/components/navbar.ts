7c0d12911a81ea2fe94166c44cee732796023101
Jayson Armbruster <Jarmbruster@jerviswebb.com>
2019-02-26 15:23:11 -0500

    Added components for navbar, and sidebar

---
+++ site/src/app/components/navbar.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'navbar',
+    templateUrl: '../views/navbar.html'
+})
+
+export class NavbarComponent {
+
+}
---
+++ site/src/app/components/sidebarInfo.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sidebar-info',
+    templateUrl: '../views/sidebarInfo.html'
+})
+
+export class SideBarInfoComponent {
+
+}
