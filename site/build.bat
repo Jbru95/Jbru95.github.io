411e1e30872a80eb7ed526b9959c4dfd0da279bd
Jayson Armbruster <Jarmbruster@jerviswebb.com>
2019-02-26 11:03:17 -0500

    added new command to package.json scripts object -> npm deploy and added this command to build.bat

---
+++ site/build.bat
@@ -0,0 +1,3 @@
+echo Building app in production mode, sending files to docs folder in repository, which is set to deploy automatically to jbru95.github.io when files are comitted.
+npm run deploy
+PAUSE
