97a0eb63d84ffe91aaa1ec7788e222c59871775e
Jayson Armbruster <Jarmbruster@jerviswebb.com>
2019-02-26 11:39:10 -0500

    master-build

--- site/build.bat
+++
@@ -1,3 +0,0 @@
-echo Building app in production mode, sending files to docs folder in repository, which is set to deploy automatically to jbru95.github.io when files are comitted.
-npm run deploy
-PAUSE
