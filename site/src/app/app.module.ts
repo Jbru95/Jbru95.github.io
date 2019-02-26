be9de2bd60200f5a8527e8458a9bd8751b378a49
Jayson Armbruster <Jarmbruster@jerviswebb.com>
2019-02-26 16:14:01 -0500

    added routing to app.module.ts, and redirecting to home page => '' from wildcard routes

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar';
 import { SideBarInfoComponent } from './components/sidebarInfo';
+import { ProjectsComponent } from './components/projects'; 
+import { HomeComponent } from './components/home';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'projects', component: ProjectsComponent},
+  { path: '**', redirectTo: ''}
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SideBarInfoComponent
+    SideBarInfoComponent,
+    ProjectsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
