b79c7444ff6598fc5e3dc3cc044d0039db14ca6b
Jayson Armbruster <Armbru43@msu.edu>
2019-03-02 19:36:10 -0500

    fixed up some css, also added nearly finished, project page, just need to fill out specific content and images and build

--- site/src/app/components/projects.ts
+++ site/src/app/components/projects.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Project, ProjectInterface } from '../models/project';
+declare var $: any;
 
 @Component({
     selector: 'projects',
@@ -11,45 +12,82 @@ import { Project, ProjectInterface } from '../models/project';
 })
 
 export class ProjectsComponent {
-
+    searchTerm: string;
     projects: Project[] = new Array();
+    allProjects: Project[] = new Array();
+    searchedProjectNumber: number;
 
     constructor( ) {
         const ProjectIntArray: ProjectInterface[] = [
             {
                 name: 'kiNG',
-                description: '',
-                link: '',
+                description: 'Chess engine written in Angular, supports both local multiplayer and single player AI modes',
+                link: 'https://jbru95.github.io/kiNG',
                 imagePath: '',
-                techs: ['TypeScript', 'Angular 6', 'BootStrap']
+                techs: ['TypeScript', 'Angular 6', 'BootStrap'],
+                searchTerms: ['Chess', 'kiNG', 'TypeScript', 'Angular 6', 'BootStrap', 'multiplayer', 'AI', 'game']
             },
             {
                 name: 'Mastermind',
-                description: '',
-                link: '',
+                description: 'Web version of the classis boardgame, Mastermind written in Javascript, utilizing JQuery, Less styles, and Grunt',
+                link: 'https://jbru95.github.io/Mastermind',
                 imagePath: '',
-                techs: ['JavaScript', 'JQuery', 'CSS', 'HTML', 'Grunt', 'Less']
+                techs: ['JavaScript', 'JQuery', 'CSS', 'HTML', 'Grunt', 'Less'],
+                searchTerms: ['Mastermind', 'JavaScript', 'JQuery', 'CSS', 'HTML', 'Grunt', 'Less', 'game', 'singleplayer']
             },
             {
-                name: 'ClosestAirplane',
-                description: '',
+                name: 'Closest Airplane',
+                description: 'Simple Command line progam to return information on the closest inflight airplanes to a user input latitude and longitude, written in C#, utlizing REST APIs',
                 link: '',
                 imagePath: '',
-                techs: ['C#', 'REST APIs']
+                techs: ['C#', 'REST APIs'],
+                searchTerms: ['Closest Airplane', 'C#', 'REST APIs']
             },
             {
                 name: 'Bummer',
-                description: '',
+                description: 'Twist on the classis board game Sorry, written mainly in PHP, utlizes extensive server side scripting, as well as JavaScript, Websockets and MySQL databases for online multiplayer',
                 link: '',
                 imagePath: '',
-                techs: ['JavaScript', 'PHP', 'Websockets']
+                techs: ['JavaScript', 'PHP', 'Websockets'],
+                searchTerms: ['Bummer', 'JavaScript', 'PHP', 'Websockets', 'Sorry', 'multiplayer', 'game']
             }
         ];
         
         ProjectIntArray.forEach(element => {
-            this.projects.push( new Project(element));
+            this.allProjects.push( new Project(element));
         });
-        
+        this.projects = this.allProjects;
         console.log(this.projects);
+        this.searchedProjectNumber = this.projects.length;
+    }
+
+    onSearchKeyUp(): void{
+        this.searchTerm = $('#projectSearchTermInput')[0].value.toLowerCase();
+        if (this.searchTerm == '') {
+            this.projects = this.allProjects;
+        }
+        this.searchedProjectNumber = this.projects.length;
+    }
+
+    searchProjects(val: any): void {
+        this.searchTerm = $('#projectSearchTermInput')[0].value.toLowerCase();
+        // console.log(this.searchTerm);
+
+        this.projects = [];
+
+        for ( let proj of this.allProjects ) {
+            for ( let projSearchTerm of proj.searchTerms ) {
+                if( projSearchTerm.toLowerCase().includes(this.searchTerm)) {
+                    this.projects.push(proj);
+                    break;
+                }
+            }
+        }
+
+        if (this.searchTerm == '') {
+            this.projects = this.allProjects;
+        }
+
+        this.searchedProjectNumber = this.projects.length;
     }
 }
--- site/src/app/models/project.ts
+++ site/src/app/models/project.ts
@@ -4,22 +4,27 @@ export interface ProjectInterface {
     link: string;
     techs: Array<string>;
     imagePath: string;
+    searchTerms: Array<string>;
 }
 
 export class Project {
     name: string;
     description: string;
     link: string;
-    techs: Array<string>;
+    techs: Array<string> = [];
     imagePath: string;
+    searchTerms: Array<string> = [];
 
     constructor(obj: ProjectInterface) {
         this.name = obj.name;
         this.description = obj.description;
         this.link = obj.link;
-        obj.techs.forEach( function( tech: string ) {
-            this.techs.push(tech);
+        obj.techs.forEach( el => {
+            this.techs.push(el);
         });
         this.imagePath = obj.imagePath;
+        obj.searchTerms.forEach ( el2 => {
+            this.searchTerms.push(el2);
+        })
     }
 }
