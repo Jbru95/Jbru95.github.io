36bf6fd1252451330f22b6ace68db5737bbf71ee
Jayson Armbruster <Jarmbruster@jerviswebb.com>
2019-03-06 13:33:14 -0500

    post build

--- site/build.bat
+++ site/build.bat
@@ -1,5 +1,5 @@
 @echo off
 title Building Jbru95.github.io
-echo Building app in production mode, sending files to docs folder in repository, which is set to deploy automatically to jbru95.github.io when files are comitted.
+echo Building app in production mode, sending files to docs folder in repository, which is set to deploy automatically to jbru95.github.io when files are committed.
 ng build --prod --deployUrl="https://jbru95.github.io/" --outputPath=./docs --base-href="https://jbru95.github.io/"
 pause
